ea6a1c9d4da385f5f433683f03d7ece38975720a
yorlysoro <yorlysoro@gmail.com>
2022-06-04 22:22:39 -0400

    Install debug toolbar

--- wagtail_test/home/models.py
+++ wagtail_test/home/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 from wagtail.models import Page
-
+from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
     """Home Page model."""
     templates = "home/home_page.html"
+    banner_tittle = models.CharField(max_length=255, blank=False, null=True)
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_tittle")
+    ]
--- wagtail_test/wagtail_test/urls.py
+++ wagtail_test/wagtail_test/urls.py
@@ -24,6 +24,11 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+    urlpatterns += [
+        # ...
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
+
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
@@ -32,4 +37,5 @@ urlpatterns = urlpatterns + [
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    path("pages/", include(wagtail_urls)),
+    
 ]
